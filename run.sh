67cff4d01682baa475e4b9000b015244a2b48e44
Jacob Oscarson <jacob@plexical.com>
2011-10-26 14:52:19 +0200

    Made the simple runner more generic, changed executable names to be *.app

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-/usr/bin/gcc-4.2 -framework Foundation hello.m -o hello
+if [ "$1" == "" ]; then
+    echo "Usage: run.sh <name of example program>"
+    exit 1
+fi
+
+/usr/bin/gcc-4.2 -framework Foundation $1.m -o $1.app
 if [  "$?" == "0" ]; then
-    ./hello
+    ./$1.app
 fi
