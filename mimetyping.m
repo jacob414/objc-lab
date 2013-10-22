65c0427e6724350bf70b2d8985c84f6cf6f7b244
Jacob Oscarson <jacob@plexical.com>
2013-10-22 16:05:45 +0200

    Added failing text for parsing encoding from MIME-type

--- mimetyping.m
+++ mimetyping.m
@@ -2,12 +2,17 @@
 
 @interface NSString (MIMEAware)
 @property (readonly, getter=isText) BOOL textual;
+@property (readonly, getter=getEncoding) NSString *encoding;
 -(BOOL)isText;
 @end
 
 @implementation NSString (MIMEAware)
 -(BOOL)isText {
   return [self hasPrefix:@"text/"];
+}
+
+-(NSString*)getEncoding {
+ return nil;
 }
 @end
 
@@ -24,6 +29,8 @@ int main(int argc, char *argv[]) {
 
   my_chk(!@"application/rtf".textual, @"application/rtf binary");
   my_chk(@"text/plain".textual, @"text/plain textual");
+  my_chk([@"application/rtf; charset=UTF-8".encoding isEqual:@"utf-8"],
+         @"RTF with encoding");
 
   return 0;
 }
