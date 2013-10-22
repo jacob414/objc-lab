28ee5be5e5c1a627b59cc179306fb88b9b060dbe
Jacob Oscarson <jacob@plexical.com>
2013-10-22 21:52:49 +0200

    Ugh... what I really needed was a NSStringEncoding

--- mimetyping.m
+++ mimetyping.m
@@ -2,9 +2,9 @@
 
 @interface NSString (MIMEAware)
 @property (readonly, getter=isText) BOOL textual;
-@property (readonly, getter=getEncoding) NSString *encoding;
+@property (readonly, getter=getEncodingOrUtf8) NSString *encodingOrUtf8;
+@property (readonly, getter=getNSEncoding) NSStringEncoding nsEncoding;
 @property (readonly, getter=getMimeType) NSString *mimeType;
--(BOOL)isText;
 @end
 
 @implementation NSString (MIMEAware)
@@ -15,15 +15,29 @@
 /**
  * See http://www.w3.org/International/O-HTTP-charset
  */
--(NSString*)getEncoding {
-  return
-    [[[self componentsSeparatedByString:@"charset="]
-       objectAtIndex:1] lowercaseString];
+-(NSString*)getEncodingOrUtf8 {
+  NSArray *parts = [self componentsSeparatedByString:@"charset="];
+  NSString *charset;
+  if ([parts count] == 2) {
+    charset = [[parts objectAtIndex:1] lowercaseString];
+  }
+  return charset != nil ? charset : @"utf-8";
 }
 
 -(NSString*)getMimeType {
   return [[self componentsSeparatedByString:@"; "] objectAtIndex:0];
 }
+
+/**
+ * Convert an IANA character set to a `NSStringEncoding`. If
+ * `encodingName` is `nil`, return `NSUTF8StringEncoding` as a guess.
+ */
+-(NSStringEncoding)getNSEncoding {
+  NSString *encName = self.encodingOrUtf8;
+  CFStringEncoding cfEnc = CFStringConvertIANACharSetNameToEncoding((CFStringRef)encName);
+  return CFStringConvertEncodingToNSStringEncoding(cfEnc);
+}
+
 @end
 
 void my_chk(bool expr, NSString *desc) {
@@ -39,11 +53,17 @@ int main(int argc, char *argv[]) {
 
   my_chk(!@"application/rtf".textual, @"application/rtf binary");
   my_chk(@"text/plain".textual, @"text/plain textual");
-  my_chk([@"application/rtf; charset=UTF-8".encoding isEqual:@"utf-8"],
+  my_chk([@"application/rtf; charset=UTF-8".encodingOrUtf8 isEqual:@"utf-8"],
          @"RTF with encoding");
+  my_chk([@"text/html".encodingOrUtf8 isEqual:@"utf-8"],
+         @"HTMP without encoding guesses on UTF-8");
   my_chk([@"text/plain".mimeType isEqual:@"text/plain"],
          @"Text plain without encoding");
   my_chk([@"text/plain; charset=UTF-8".mimeType isEqual:@"text/plain"],
          @"Text plain with encoding");
+  my_chk(@"text/html".nsEncoding == NSUTF8StringEncoding,
+         @"text/html without encoding guesses NSUTF8StringEncoding");
+  my_chk(@"text/html; charset=UTF-16".nsEncoding == NSUTF16StringEncoding,
+         @"text/html with encoding");
   return 0;
 }
